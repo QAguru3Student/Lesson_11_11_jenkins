fd4e19503515b677a384a145eb2fd7f715d39413
Babushkin Anton <napishi_antonu@mail.ru>
2022-04-28 20:24:17 +0300

    add demoqa tests

---
+++ src/test/java/one/pages/RegistrationsPage.java
@@ -0,0 +1,14 @@
+package one.pages;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationsPage {
+
+    //locators & elements
+
+    //actions
+    public void openPage() {
+        open("https://demoqa.com/automation-practice-form");
+    }
+
+}
